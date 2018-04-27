b7bc97b0b6aae728f3669cea4e3840c3cb5eaacc
Lynette <lyna8907@gmail.com>
2018-04-27 15:04:08 -0500

    first commit

---
+++ src/main/java/com/kincurrently/KincurrentlyApplication.java
@@ -0,0 +1,12 @@
+package com.kincurrently;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KincurrentlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KincurrentlyApplication.class, args);
+	}
+}
