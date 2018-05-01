e02abec80e0f64ba056051237cb5cbb2ec166993
Lynette <lyna8907@gmail.com>
2018-05-01 15:56:38 -0500

    added build config file, changes to application file

--- src/main/java/com/kincurrently/KincurrentlyApplication.java
+++ src/main/java/com/kincurrently/KincurrentlyApplication.java
@@ -2,11 +2,16 @@ package com.kincurrently;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KincurrentlyApplication {
+public class KincurrentlyApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KincurrentlyApplication.class, args);
+	}
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KincurrentlyApplication.class);
 	}
 }
