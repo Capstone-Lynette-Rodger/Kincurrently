3c46f32dc407b5581feb00f8f755729cd610f829
Lynette <lyna8907@gmail.com>
2018-05-02 13:30:11 -0500

    added js for views

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -5,13 +5,44 @@ let dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
 currentDate = dayArray[current.getUTCDay()] + ", " + monthArray[current.getUTCMonth()] + " " + current.getUTCDate() + ', ' + current.getUTCFullYear();
 $(".todaysDate").text(currentDate);
 
-$.each($(".changeDate"), (index, element) => {
-    let date = new Date(element.textContent);
-    element.innerHTML = dayArray[date.getUTCDay()] + ", " + monthArray[date.getUTCMonth()] + " " + date.getUTCDate();
-    if(current.getUTCFullYear() !== date.getUTCFullYear()) {
-        element.innerHTML += ', ' + date.getUTCFullYear();
-    }
-});
+// $.each($(".changeDate"), (index, element) => {
+//     let date = new Date(element.textContent);
+//     element.innerHTML = dayArray[date.getUTCDay()] + ", " + monthArray[date.getUTCMonth()] + " " + date.getUTCDate();
+//     if(current.getUTCFullYear() !== date.getUTCFullYear()) {
+//         element.innerHTML += ', ' + date.getUTCFullYear();
+//     }
+// });
+
+let changeView = () => {
+    $.each($('.date'), (index, element) => {
+        let date = new Date(element.textContent);
+        console.log(element.textContent);
+        console.log(date);
+        switch($('#taskView').val()) {
+            case "day":
+                if(current.getUTCFullYear() !== date.getUTCFullYear() || current.getUTCMonth() !== date.getUTCMonth() || current.getUTCDate() !== date.getUTCDate()) {
+                    let task = element.parentNode.parentNode;
+                    task.parentNode.setAttribute("hidden");
+                }
+                break;
+            case "week":
+                console.log(current+7);
+                if(date > current + 7  || date < current) {
+                    let task = element.parentNode.parentNode;
+                    task.parentNode.removeChild(task);
+                }
+                break;
+            case "month":
+                if(current.getUTCFullYear() !== date.getUTCFullYear() || date.getUTCMonth() !== current.getUTCMonth()) {
+                    let task = element.parentNode.parentNode;
+                    task.parentNode.removeChild(task);
+                }
+                break;
+        }
+    });
+};
+changeView();
+$('#taskView').change(changeView());
 
 $('#joinIfExisting').click(()=> {
     $('#joinIfExisting').attr('checked', function(index, attr){
