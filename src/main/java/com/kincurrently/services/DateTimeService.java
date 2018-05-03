323af83a52d8cbc2b0c8745b9da9d06c15c4e966
Lynette <lyna8907@gmail.com>
2018-05-03 15:05:13 -0500

    added date sorting to dtservice

--- src/main/java/com/kincurrently/services/DateTimeService.java
+++ src/main/java/com/kincurrently/services/DateTimeService.java
@@ -1,10 +1,14 @@
 package com.kincurrently.services;
 
+import com.kincurrently.models.Event;
+import com.kincurrently.models.Task;
 import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class DateTimeService {
@@ -29,5 +33,14 @@ public class DateTimeService {
         return date;
     }
 
+    public List<Task> sortTasksByDate(List<Task> tasks) {
+        tasks.sort(Comparator.comparing(o -> o.getCompleted_by()));
+        return tasks;
+    }
+
+    public List<Event> sortEventsByDate(List<Event> events) {
+        events.sort(Comparator.comparing(o -> o.getStart_date()));
+        return events;
+    }
 
 }
